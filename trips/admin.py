9b18f27509e443298a3b62407f15d5fae12c9f1e
makssukhum-lgtm <maks.sukhum@gmail.com>
2025-09-05 12:09:41 +0300

    feat: Setup Django admin for all models

--- trips/admin.py
+++ trips/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Vehicle, Trip, Booking
 
-# Register your models here.
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    """
+    Настройки отображения транспорта.
+    """
+    list_display = ('__str__', 'driver')
+    search_fields = ('brand', 'model', 'license_plate', 'driver__name')
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    """
+    Настройки отображения поездок.
+    """
+    list_display = (
+        'departure_location',
+        'destination_location',
+        'departure_time',
+        'driver',
+        'vehicle',
+        'available_seats'
+    )
+    list_filter = ('departure_location', 'destination_location')
+    search_fields = ('departure_location', 'destination_location', 'driver__name')
+    date_hierarchy = 'departure_time'
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """
+    Настройки отображения бронирований.
+    """
+    list_display = ('passenger', 'trip', 'seats_booked', 'created_at')
+    search_fields = ('passenger__name', 'trip__driver__name')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    Настройки отображения пользователей в админ-панели.
+    """
+    list_display = ('name', 'telegram_id', 'phone_number', 'role', 'language')
+    list_filter = ('role', 'language')
+    search_fields = ('name', 'telegram_id', 'phone_number')
+    list_per_page = 20
