d5f41690f08f633f43df029244a180289ac37b1e
makssukhum-lgtm <maks.sukhum@gmail.com>
2025-09-05 12:50:48 +0300

    feat: Configure project for PostgreSQL and deployment

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config # <-- Импортируем новую библиотеку
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -7,28 +8,31 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^9#*o$6)3(z&6z-p_p*p)w#k@b$i$v@a*g(u#f#*g*p(y*w#o'
+# SECRET_KEY теперь тоже берется из .env файла
+SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG теперь управляется из .env файла
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+# ДОБАВЛЯЕМ ВАШ СЕРВЕР И ДОМЕН В РАЗРЕШЕННЫЕ ХОСТЫ
+ALLOWED_HOSTS = [
+    '188.120.234.34',
+    'myroute.fvds.ru',
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    # JAZZMIN ДОЛЖЕН БЫТЬ ПЕРВЫМ
     'jazzmin',
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     # Наши приложения
     'users.apps.UsersConfig',
     'trips.apps.TripsConfig',
@@ -69,10 +73,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# ПОЛНОСТЬЮ НОВЫЙ БЛОК ДЛЯ РАБОТЫ С POSTGRESQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT', default='5432'),
     }
 }
 
@@ -81,18 +90,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
+    { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
+    { 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
+    { 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
 ]
 
 
@@ -100,11 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'ru-ru'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
@@ -112,7 +110,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# ДОБАВЛЯЕМ ПУТЬ ДЛЯ СБОРА СТАТИКИ
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
@@ -123,24 +120,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # --- НАСТРОЙКИ JAZZMIN ---
-
 JAZZMIN_SETTINGS = {
-    # Заголовок, который будет отображаться вверху страницы
     "site_title": "MyRoute Admin",
-
-    # Короткий заголовок для боковой панели
     "site_header": "MyRoute",
-
-    # Логотип (пока оставим пустым, можно добавить позже)
     "site_logo": None,
-
-    # Текст на экране входа
     "welcome_sign": "Добро пожаловать в панель управления MyRoute",
-
-    # Авторские права в подвале
     "copyright": "MyRoute Ltd.",
-
-    # Включаем темную тему
     "dark_mode_theme": "darkly",
 }
 
